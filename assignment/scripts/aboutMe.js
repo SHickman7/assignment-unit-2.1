f268f32b2ece2f6953b1598ee7157e1fd85ecf4e
Stephanie Hickman <stephick@gmail.com>
2023-12-06 07:25:54 -0600

    Question 2 - created lastName variable

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -1,9 +1,11 @@
 // REQUIRED FEATURES:
 // 1 - Create a variable called `firstName` and assign it the value of your first name
 let firstName = 'Stephanie';
-console.log("My first name is",firstName);
+console.log( "My first name is",firstName );
 
 // 2 - Create a second variable called `lastName` and assign it the value of your last name
+let lastName = 'Hickman';
+console.log( "My last name is",lastName )
 
 // 3 - Create a third variable called `fullName`, assign it the value of your first and last name
 //     (remember, you already have variables for this, can you use those?)
