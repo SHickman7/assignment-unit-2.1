6a1dab64bc8681f71d0d8d9a195cd9f5dc883637
Stephanie Hickman <stephick@gmail.com>
2023-12-06 07:31:52 -0600

    Question 3 - created fullName variable

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -10,6 +10,8 @@ console.log( "My last name is",lastName )
 // 3 - Create a third variable called `fullName`, assign it the value of your first and last name
 //     (remember, you already have variables for this, can you use those?)
 //     Console log the value of `fullName`
+let fullName = firstName + ' ' + lastName;
+console.log( "My full name is", fullName);
 
 // 4 - Create a variable called `luckyNumber` and assign it the value of your lucky number.
 //     Console log the value of `luckyNumber`
